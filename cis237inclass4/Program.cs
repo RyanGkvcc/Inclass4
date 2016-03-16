dae372f2b23f93fdb39e3c7b8045a1d9dcf9f2e4
Ryan Gowan <tr_jester@hotmail.com>
2016-03-16 19:54:17 -0400

    Add GenericLinkedList and GenericNode

--- cis237inclass4/Program.cs
+++ cis237inclass4/Program.cs
@@ -54,6 +54,82 @@ namespace cis237inclass4
             {
                 Console.WriteLine(x.Data);
             }
+
+
+            //Couple of blank lines
+            Console.WriteLine();
+            Console.WriteLine();
+            //Couple of blank lines
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //
+            //
+            //
+            
+
+            //Make a linked list that keeps track of strings just like the other one we did.
+            GenericLinkedList<string> myGenericLinkedList = new GenericLinkedList<string>();
+            //Make a linked list that keeps track of ints.
+            GenericLinkedList<int> myGenericIntegerLinkedList = new GenericLinkedList<int>();
+            //Make a linked list that keeps track of objects. This is probably not a good idea.
+            GenericLinkedList<object> myGenericObjectLinkedList = new GenericLinkedList<object>();
+            //Make a linked list that keeps track of integer arrays.
+            GenericLinkedList<int[]> myGenericIntegerArrayLinkedList = new GenericLinkedList<int[]>();
+
+
+            //Add a few things to the linked list
+            myGenericLinkedList.Add("first");
+            myGenericLinkedList.Add("second");
+            myGenericLinkedList.Add("third");
+            myGenericLinkedList.Add("fourth");
+
+            //Loop through with this differently looking for loop to output.
+            //In here, the first part is initialization: Setting x to the Head
+            //the second part is the test: if x!= null, keep going
+            //The last part is: Set the current x to x's next pointer. (The next in the list)
+            for (GenericNode<string> x = myGenericLinkedList.Head; x != null; x = x.Next)
+            {
+                Console.WriteLine(x.Data);
+            }
+
+            //Couple of blank lines
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Use the retrive method to start at the Head of the linked list,
+            //walk through it, and return the value that is at the passed in index.
+            Console.WriteLine(myGenericLinkedList.Retrive(2));
+            Console.WriteLine(myGenericLinkedList.Retrive(1));
+
+            //Couple of blank lines
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //Do some deletes of the linked list
+            //This will delete the frst one in the list
+            bool success = myGenericLinkedList.Delete(0);
+            if (success)
+            {
+                Console.WriteLine("Deleted Successfully");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+            //Now there are 3, and this will delete the middle one
+            myGenericLinkedList.Delete(1);
+            //Now there are 2, and this will delete the middle one
+            myGenericLinkedList.Delete(1);
+            //Now there is only 1, and this loop will print it out
+            for (GenericNode<string> x = myGenericLinkedList.Head; x != null; x = x.Next)
+            {
+                Console.WriteLine(x.Data);
+            }
+
+
+
         }
     }
 }
